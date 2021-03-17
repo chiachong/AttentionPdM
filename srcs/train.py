7063eac22aa44d3aa19376a629c3d987bd40dfe3
ChiaChong <chiachong.14@gmail.com>
2021-03-17 23:42:32 +0800

    Integrate MLflow to track experiments

--- srcs/train.py
+++ srcs/train.py
@@ -1,18 +1,22 @@
-import os
 import sys
 import numpy as np
+import mlflow.keras
+from mlflow.tracking import MlflowClient
 sys.path.append('srcs')
 import utils
 import dataset
 from keras_models import AttentionModel
 
+mlflow.keras.autolog()
+
 if __name__ == '__main__':
+    # settings for data preprocessing
     INPUR_DIR = 'data/CMAPSSData'
-    TASK = 'regression'
+    TASK = 'classification'
     WINDOW_SIZE = 30  # scanning window size
     FAIL_IN = 14  # turbofan engine fails in 14 cycle
     TRAIN_PROPORTION = 3 / 4  # proportion of the training data
-    CLIP_RUL = 125
+    CLIP_RUL = 110
     # load and preprocess data
     data = dataset.TurbofanData(INPUR_DIR)
     data.preprocess(clip_RUL=CLIP_RUL)
@@ -20,15 +24,27 @@ if __name__ == '__main__':
     if TASK == 'classification':
         arrays = data.arrays_for_classification(WINDOW_SIZE, FAIL_IN)
         train_x, train_y, test_x, test_y, val_x, val_y = arrays
+        indexes = np.where(train_y == 0)[0]
+        indexes1 = np.where(train_y == 1)[0]
+        np.random.seed(1234)
+        np.random.shuffle(indexes)
+        train_x = np.concatenate((train_x[indexes[:3000]], train_x[indexes1]))
+        train_y = np.concatenate((train_y[indexes[:3000]], train_y[indexes1]))
         num_class = len(np.unique(train_y))
         # one-hot-encoding for classification task
         train_y = utils.one_hot_encode(train_y, num_class)
         test_y = utils.one_hot_encode(test_y, num_class)
         val_y = utils.one_hot_encode(val_y, num_class)
+        experiment_id = '1'
+        # check whether there is mlflow experiment created for classification
+        client = MlflowClient()
+        if not any([e.experiment_id == '1' for e in client.list_experiments()]):
+            mlflow.create_experiment("Classification")
     else:
         arrays = data.arrays_for_regression(WINDOW_SIZE)
         train_x, train_y, test_x, test_y, val_x, val_y = arrays
         num_class = 1
+        experiment_id = '0'
 
     # experiment and model settings
     args = {
@@ -41,17 +57,33 @@ if __name__ == '__main__':
         'dropout_rate': 0.1,
         'batch_normalization': True,
         'batch_size': 256,
-        'epochs': 100,
-        'eval_per_epoch': 3,
+        'epochs': 40,
+        'eval_per_epoch': 5,
+        'learning_rate_decay': 0.01,
     }
+    # log experiment and model settings
+    with mlflow.start_run(experiment_id=experiment_id):
+        mlflow.log_param('task', TASK)
+        mlflow.log_param('train_proportion', TRAIN_PROPORTION)
+        mlflow.log_param('clip_RUL', CLIP_RUL)
+        mlflow.log_params(args)
 
-    # instantiate model
-    pdm_model = AttentionModel(task=TASK, args=args)
-    try:
+        # instantiate model
+        pdm_model = AttentionModel(task=TASK, args=args)
+        mlflow.log_param('model_type', pdm_model._model_name)
         pdm_model.train(train=[train_x, train_y],
-                        val=[test_x, test_y],
+                        val=[val_x, val_y],
                         batch_size=args['batch_size'],
                         epochs=args['epochs'],
-                        eval_per_epoch=args['eval_per_epoch'])
-    except KeyboardInterrupt:
-        pdm_model.test(val_x, val_y)
+                        eval_per_epoch=args['eval_per_epoch'],
+                        learning_rate_decay=args['learning_rate_decay'])
+        if TASK == 'regression':
+            loss, mae = pdm_model.test(test_x, test_y)
+            # log model performance
+            mlflow.log_metric('test_loss', loss)
+            mlflow.log_metric('test_mean_absolute_error', mae)
+        else:
+            precision, recall, f1 = pdm_model.test(test_x, test_y)
+            mlflow.log_metric('precision', precision)
+            mlflow.log_metric('recall', recall)
+            mlflow.log_metric('f1-score', f1)
